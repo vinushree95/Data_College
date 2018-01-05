11ca9a3b1c31bf9d41f5492ac71ce816d3e724e3
vinushree95 <32702697+vinushree95@users.noreply.github.com>
2018-01-05 23:07:00 +0530

    Update connect.php

--- labs/connect.php
+++ labs/connect.php
@@ -1,6 +1,6 @@
 
 <?php 
-$mysqli = new mysqli("freshapp", "umeravinu", "Nafisa786", "mystudent");
+$mysqli = new mysqli("freshapp.database.windows.net", "umeravinu", "Nafisa786", "mystudent");
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
